73d2ce222fbf5fd7d23d507e58fc888d4d375737
Tallerlei <mtallarek@gmail.com>
2016-02-24 18:43:27 +0100

    first automation processes added
    scss to css
    prefixer for browserversions

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  // prefixes css to match browser specification
+  autoprefixer = require('gulp-autoprefixer'),
+  // transpiles scss into css
+  sass = require('gulp-sass');
 
-gulp.task('default', function(){
+gulp.task('process-styles', function() {
+  return gulp.src('src/styles/main.scss')
+    .pipe(sass({
+      style: 'expanded'
+    }))
+    .pipe(autoprefixer({
+      // Which Browserversions should be included?
+      browsers: ['last 2 versions', '> 5%', 'ie 9', 'ie 10'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dest/styles/'));
+});
+
+gulp.task('default', function() {
+
   console.log("I have configured a gulp file!");
 });
